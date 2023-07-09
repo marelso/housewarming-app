07462204de1a40b1057a2a4853104fa41546f2e5
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-07-09 10:19:28 -0300

    Include module for di

--- app/src/main/java/com/tah/housewarming/di/module.kt
+++ app/src/main/java/com/tah/housewarming/di/module.kt
@@ -1,9 +1,8 @@
 package com.tah.housewarming.di
 
-import com.google.gson.GsonBuilder
 import com.tah.housewarming.data.PinterestService
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
+import com.tah.housewarming.ui.dashboard.DashboardViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -16,18 +15,14 @@ private const val PINTEREST_API_URL = "http://widgets.pinterest.com/"
 private const val PINTEREST_API = "PINTEREST_API"
 
 private fun provideApi(url: String): Retrofit {
-
-    val gson = GsonBuilder().setLenient().create()
-
-    val client = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+    return Retrofit.Builder()
+        .baseUrl(url)
+        .addConverterFactory(GsonConverterFactory.create())
         .build()
-
-    return Retrofit.Builder().baseUrl(url).client(client)
-        .addConverterFactory(GsonConverterFactory.create(gson)).build()
 }
 
 val module = module {
-    factory(named(PINTEREST_API)) { provideApi(PINTEREST_API_URL) }
+    single(named(PINTEREST_API)) { provideApi(PINTEREST_API_URL) }
     factory { get<Retrofit>(named(PINTEREST_API)).create(PinterestService::class.java) }
+    viewModel { DashboardViewModel(get()) }
 }
