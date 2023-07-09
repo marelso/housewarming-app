1bb7a7bfe9e8529599257c7a5c60952c43a4735a
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-07-09 10:18:48 -0300

    Updating interface methods

--- app/src/main/java/com/tah/housewarming/data/PinterestService.kt
+++ app/src/main/java/com/tah/housewarming/data/PinterestService.kt
@@ -1,15 +1,17 @@
 package com.tah.housewarming.data
 
+import com.tah.housewarming.ui.dashboard.PinInfoResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PinterestService {
 
-    @GET("v3/pidgets/pins/info/?pin_ids={pinId}")
-    suspend fun getPins(@Query("pinId") pinId: String)
+    @GET("/v3/pidgets/pins/info/")
+    suspend fun getPins(@Query("pin_ids") pinIds: String): Response<PinInfoResponse>
 
     @GET("v3/pidgets/users/{userId}/pins/")
-    suspend fun getUserPins(@Path("userId") userId: String)
+    suspend fun getUserPins(@Path("userId") userId: String): Response<PinInfoResponse>
 
 }
