4f3913c79cb0c28be98460fc0da0bad3e7eeaf5d
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-07-09 10:19:11 -0300

    Include view model with service reference

--- app/src/main/java/com/tah/housewarming/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/tah/housewarming/ui/dashboard/DashboardViewModel.kt
@@ -1,4 +1,26 @@
 package com.tah.housewarming.ui.dashboard
 
-class DashboardViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tah.housewarming.data.PinterestService
+import kotlinx.coroutines.launch
+
+class DashboardViewModel(private val service: PinterestService) : ViewModel() {
+
+    fun setup() {
+
+        viewModelScope.launch {
+            val userPins = service.getPins("833869687245603658")
+
+            val code = userPins.code()
+
+            when(code) {
+                400 -> {}
+                500 -> {}
+                else -> {}
+            }
+        }
+
+    }
+
 }
