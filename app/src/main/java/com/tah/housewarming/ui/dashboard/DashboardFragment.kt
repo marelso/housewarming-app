0899a4fa6333995d1057a986e8c23292f897c91f
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-07-09 11:14:17 -0300

    Removing unused import and viewModel setup method

--- app/src/main/java/com/tah/housewarming/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/tah/housewarming/ui/dashboard/DashboardFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
 import com.tah.housewarming.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -46,7 +45,6 @@ class DashboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        dashboardViewModel.setup()
     }
 
     companion object {
