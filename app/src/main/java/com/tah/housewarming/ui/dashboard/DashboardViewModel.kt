4778c145dfef67eb32557a310e95ff12be25adc0
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-07-10 14:58:05 -0300

    Including state at viewModel

--- app/src/main/java/com/tah/housewarming/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/tah/housewarming/ui/dashboard/DashboardViewModel.kt
@@ -1,11 +1,17 @@
 package com.tah.housewarming.ui.dashboard
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tah.housewarming.data.Status
 import com.tah.housewarming.data.pinterest.PinterestRepository
 import kotlinx.coroutines.launch
 
 class DashboardViewModel(private val repository: PinterestRepository) : ViewModel() {
+
+    private val _userPinsUIState = MutableLiveData<UserPinsUIState>()
+    val userPinsUIState: LiveData<UserPinsUIState> = _userPinsUIState
 
     init {
         setup()
@@ -13,12 +19,24 @@ class DashboardViewModel(private val repository: PinterestRepository) : ViewMode
 
     private fun setup() {
         viewModelScope.launch {
-            val pin = repository.getPin("833869687245603658")
+            _userPinsUIState.value = UserPinsUIState(status = Status.RUNNING)
 
-            pin?.let {
-                println(pin.images)
+            try {
+                val pin = repository.getPin("833869687245603658")
+
+                val userPins = repository.getUserPins("barbieritata")
+
+                _userPinsUIState.value = UserPinsUIState(status = Status.SUCCESS, userPins)
+            } catch (e: Exception) {
+                e.message
             }
+
         }
     }
 
 }
+
+data class UserPinsUIState(
+    val status: Status,
+    val pins: List<Pin>? = null
+)
