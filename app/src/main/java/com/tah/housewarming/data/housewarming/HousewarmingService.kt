001bcce3056c0eb69a817c95cf25bca245ff52fb
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-08-01 02:24:37 -0300

    Including get methods to housewarming service.

--- app/src/main/java/com/tah/housewarming/data/housewarming/HousewarmingService.kt
+++ app/src/main/java/com/tah/housewarming/data/housewarming/HousewarmingService.kt
@@ -1,4 +1,14 @@
 package com.tah.housewarming.data.housewarming
 
+import com.tah.housewarming.data.Category
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface HousewarmingService {
+    @GET("v1/categories/")
+    suspend fun getCategories(): Response<List<Category>>
+
+    @GET("v1/categories/{id}")
+    suspend fun getCategory(@Path("id") category: Int)
 }
